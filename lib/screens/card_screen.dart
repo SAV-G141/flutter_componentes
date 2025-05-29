b85e73d81c9077a67376e7a812545fa688c5e4e2
odenfis <wmoncada@tecsup.edu.pe>
2025-05-29 11:05:28 -0500

    29052025_v3

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widget.dart';
+
 class CardScreen extends StatelessWidget {
    
   const CardScreen({super.key});
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Card Widget', style: TextStyle(color: Colors.white)),
+      ) ,
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: [
+          CustomCardType1(),
+          CustomCardType1(),
+          CustomCardType1(),
+          CustomCardType1(),
+          CustomCardType1(),
+          CustomCardType1(),
+          CustomCardType1(),
+          CustomCardType1(),
+        ],
       ),
     );
   }
 }
+
---
+++ lib/widgets/widget.dart
@@ -0,0 +1,2 @@
+export 'package:fl_componentes/widgets/custom_card_type1.dart';
+
