dbe7a9acfe122b35ffb413d71373b0b3f046aacb
odenfis <wmoncada@tecsup.edu.pe>
2025-06-12 10:09:37 -0500

    120625_v1

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -3,12 +3,28 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({super.key});
+
+  void displayDialog(BuildContext context){
+    showDialog(
+      context: context, 
+      builder:(context) => AlertDialog(
+        title: Text('Alerta de Proceso'),
+        content: Column(
+          children: [
+            Text('Esto es el contenido interno de la alerta')
+          ],
+        ),
+      ),
+      );
+  }
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-         child: Text('AlertScreen'),
+         child: ElevatedButton(
+          onPressed: (){}, 
+          child: Text('Procesar')),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){},
